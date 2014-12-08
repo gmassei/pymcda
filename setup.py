8a25608db766ea90f3dc40e335b3917aab3c0867
gianluca <agr.gianluca.massei@gmail.com>
2014-12-08 22:05:18 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pymcda',
+      version='0.1',
+      description='',
+      url=' https://github.com/gmassei/pymcda',
+      author='gianluca massei',
+      author_email='g_massa@libero.it',
+      license='GPL',
+      packages=['pymcda'],
+      zip_safe=False)
