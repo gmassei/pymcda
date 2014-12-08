509f44257d6e6129981ee2347dfa506c231b68fd
gianluca <agr.gianluca.massei@gmail.com>
2014-12-08 22:15:54 +0100

    Creating The Scaffolding¶

--- pymcda/setup.py
+++ pymcda/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='pymcda',
       version='0.1',
       description='',
-      url=' https://github.com/gmassei/pymcda',
+      url='https://github.com/gmassei/pymcda',
       author='gianluca massei',
       author_email='g_massa@libero.it',
       license='GPL',
